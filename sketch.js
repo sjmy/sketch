746a29b55c70806ce2dacc4272fdcf5e57b25e36
sjmy <23250749+sjmy@users.noreply.github.com>
2025-05-10 12:08:11 -0700

    Grid creates proper squares, scales to container.

--- sketch.js
+++ sketch.js
@@ -1,13 +1,24 @@
 const container = document.querySelector(".container");
 
-for (r = 1; r <= 8; r++) {
+for (r = 0; r < 16; r++) {
     const newRow = document.createElement("div");
     newRow.classList.add("row");
+    let rowHeight = 100 / 16;
+    newRow.style.height = `${rowHeight}%`
     container.appendChild(newRow);
 
-    for (s = 1; s <= 8; s++) {
-        const newSquare = document.createElement("div");
+    for (s = 0; s < 16; s++) {
+       const newSquare = document.createElement("div");
         newSquare.classList.add("square");
+        let sideWidth = 100 / 16;
+        newSquare.style.width = `${sideWidth}%`
         newRow.appendChild(newSquare);
     };
 };
+
+const square = document.querySelector(".square");
+
+square.addEventListener("mouseover", (e) => {
+    let target = e.target;
+    target.style.backgroundColor = "blue";
+});
